f4cfadaeaaf7aba36490daecfbed3983b16dbdb4
Eesha2468 <eeshaahsan24@gmail.com>
2026-07-15 13:37:40 +0500

    error fixed

--- server/index.js
+++ server/index.js
@@ -1562,7 +1562,16 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 // Fallback to index.html for SPA routing
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  const indexPath = path.join(__dirname, '../dist/index.html');
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.status(200).send('SLA Management System API is running.');
+      } else {
+        res.status(500).send('Error loading frontend assets.');
+      }
+    }
+  });
 });
 
 startServer();
